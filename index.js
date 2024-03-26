e57844d00c9079374b7190e1b679a0a3ba3dccb1
yassershahofficial <geniususer4u@gmail.com>
2024-03-26 17:20:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Navbar scroll effect (Enable hidden navbar)
+{
+    const nav = document.querySelector(".navbar");
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", () => {
+        if(lastScrollY < window.scrollY){
+            nav.classList.add("nav--hidden");
+        }
+        else{
+            nav.classList.remove("nav--hidden");
+        }
+
+        lastScrollY = window.scrollY;
+    })
+
+}
