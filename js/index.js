323ba6779f648211e332775742e56959e63b40e8
yassershahofficial <geniususer4u@gmail.com>
2024-04-04 18:01:03 +0800

    swiper js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// Navbar scroll effect (Enable hidden navbar)
+{
+    const nav = document.querySelector(".navbar");
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", () => {
+        if(lastScrollY < window.scrollY){
+            nav.classList.add("nav--hidden");
+        }
+        else{
+            nav.classList.remove("nav--hidden");
+        }
+
+        lastScrollY = window.scrollY;
+    })
+
+}
+// Initialize Swiper
+{
+    var swiper = new Swiper(".swiper-socialproof", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        rewind: true,
+        slidesPerView: "2.5",
+        coverflowEffect: {
+            rotate: 180,
+            stretch: 0,
+            depth: 10,
+            modifier: 0.3,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+}
