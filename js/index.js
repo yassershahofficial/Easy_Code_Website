644b009fc53e68c027563092f4d1bdb9b28aaf55
yassershahofficial <geniususer4u@gmail.com>
2024-04-17 15:35:01 +0800

    styling & reDirect

--- js/index.js
+++ js/index.js
@@ -53,25 +53,3 @@
         }
     }
 }
-//Initialize Email JS
-{
-    (function() {
-        // https://dashboard.emailjs.com/admin/account
-        emailjs.init({
-          publicKey: "3Ao3OPJnEWvI4P3t5",
-        });
-    })();
-
-    window.onload = function() {
-        document.getElementById('contact-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            // these IDs from the previous steps
-            emailjs.sendForm('service_0qtlvf9', 'template_zu8jgiy', this)
-                .then(() => {
-                    console.log('SUCCESS!');
-                }, (error) => {
-                    console.log('FAILED...', error);
-                });
-        });
-    }
-}
