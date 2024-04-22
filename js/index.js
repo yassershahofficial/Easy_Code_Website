d3da7363a1fe984035d6c798d3025bb502483045
yassershahofficial <geniususer4u@gmail.com>
2024-04-22 12:20:17 +0800

    media query social-proof & footer, nav onclick, swiper js optimization

--- js/index.js
+++ js/index.js
@@ -15,14 +15,58 @@
     })
 
 }
+//Detect the width of Web Page
+{
+    var SlidesValue =  "2.5";
+    window.addEventListener("resize", () => {
+        let widthWebPage = document.body.offsetWidth;
+        if(widthWebPage >= 768){
+            console.log("bigger");
+            SlidesValue =  "2.5";
+        }
+        else{
+            console.log("smaller");
+            SlidesValue =  "1.5";
+        }
+        var swiper = new Swiper(".swiper-socialproof", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            rewind: true,
+            slidesPerView: SlidesValue,
+            coverflowEffect: {
+                rotate: 180,
+                stretch: 0,
+                depth: 10,
+                modifier: 0.3,
+                slideShadows: true,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    })
+}
 // Initialize Swiper
 {
+    var SlidesValue =  "2.5";
+    if(document.body.offsetWidth >= 768){
+        SlidesValue =  "2.5";
+    }
+    else{
+        SlidesValue =  "1.5";
+    }
     var swiper = new Swiper(".swiper-socialproof", {
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
         rewind: true,
-        slidesPerView: "2.5",
+        slidesPerView: SlidesValue,
         coverflowEffect: {
             rotate: 180,
             stretch: 0,
@@ -39,7 +83,6 @@
             prevEl: ".swiper-button-prev",
         },
     });
-
 }
 //hidden contact form & request alert
 {
@@ -55,16 +98,18 @@
 }
 //toggle right navbar 
 {
-    function toggleShowNav(){
-        var rightNavbar =  document.getElementById("right-navbar");
-        var removeRightNavbar =  document.getElementById("remove-right-navbar");
-        if(rightNavbar.style.visibility === "hidden"){
-            rightNavbar.style.visibility = "visible";
-            removeRightNavbar.style.visibility = "visible";
+    if(document.body.offsetWidth <= 768){
+        function toggleShowNav(){
+            var rightNavbar =  document.getElementById("right-navbar");
+            var removeRightNavbar =  document.getElementById("remove-right-navbar");
+            if(rightNavbar.style.visibility === "hidden"){
+                rightNavbar.style.visibility = "visible";
+                removeRightNavbar.style.visibility = "visible";
+            }
+            else{
+                rightNavbar.style.visibility = "hidden";
+                removeRightNavbar.style.visibility = "hidden";
+            }
         }
-        else{
-            rightNavbar.style.visibility = "hidden";
-            removeRightNavbar.style.visibility = "hidden";
-        }
-    }
+    }   
 }
